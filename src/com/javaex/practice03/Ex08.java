cbdbfcf45d0d449e91bb8660bd4b3269a1e78c95
'YounghoonLEE' <'koon270692@gmail.com'>
2020-11-18 12:59:12 +0900

    8번 수정

--- src/com/javaex/practice03/Ex08.java
+++ src/com/javaex/practice03/Ex08.java
@@ -2,10 +2,13 @@ package com.javaex.practice03;
 
 public class Ex08 {
 	public static void main(String[] args) {
-		for(int k=2; k<=9; k++) {
-			for(int i=1; i<=9; i++) {
-				System.out.println(k+"*"+i+"="+(k*i));
+		for(int k=1; k<=9; k++) {
+			for(int i=2; i<=9; i++) {
+				System.out.printf("%d*%d=%-5d",i,k,i*k);
+				
 			}
+			System.out.println();
+			
 		}
 		
 	}
